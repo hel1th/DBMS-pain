94bd28d04075f0c86b931117a60d594cbcb0d3a0
hel1th <68184570+hel1th@users.noreply.github.com>
2026-05-25 12:53:16 +0300

    Feature/Index  (#5)
    * base class with search, some iterators. need constructors, insert, remove and minor code style changes
    
    * get rid of stupid files
    
    * i go to linux. everything is broken
    
    * base class with search, some iterators. need constructors, insert, remove and minor code style changes
    
    * get rid of stupid files
    
    * i go to linux. everything is broken
    
    * reformat
    
    * refactor CMake configuration: update project structure, remove obsolete index CMakeLists, and enhance include directories
    
    * code style corrected, bsp constructors corrected
    
    * code and constructor changes
    
    * iterators and constructors
    
    * some iterators and constructors
    
    * bounds are corrected
    
    * b*+ tree is READY
    
    * added wide tests, need more b*+ testing
    
    * need to find mistake in erase. big amount of data crashes. i m so exhausted i want to die
    
    * fixed 2 edge cases, need more
    
    * READY!!!!!!!!!!!!!!
    
    * tests with 30000 emplaces and 1000 erases are passed.
    
    * add(indx-manager): new interface
    
    * index manager interface
    
    * ?
    
    * need load and save
    
    * need pull request
    
    * code is ready, but falls on test3
    
    * indexManager is ready
    
    ---------
    
    Co-authored-by: Vika <vika.yefremova.06@gmail.com>
    Co-authored-by: Vladimir Antygin <vl.antygin@gmail.com>
    # Conflicts:
    #	CMakeLists.txt
    #	src/index/BPlusTree.h
    #	src/index/IndexManager.cpp
    #	src/index/IndexManager.h
    #	src/storage/PageManager.h
    #	src/storage/RecordManager.h
    #	src/storage/Serializer.h

--- src/storage/RecordManager.h
+++ src/storage/RecordManager.h
@@ -5,16 +5,16 @@
 
 #include "PageManager.h"
 #include "Serializer.h"
-#include "engine/Schema.h"
-#include "utils/Value.h"
+#include "../engine/Schema.h"
+#include "../utils/Value.h"
 
 // Идентификатор записи = номер страницы + номер слота на странице
 struct RecordID {
-    page_id_t page_id;
-    int16_t slot_id;
+    page_id_t pageID;
+    int16_t   slotID;
 
     bool operator==(const RecordID& o) const {
-        return page_id == o.page_id && slot_id == o.slot_id;
+        return pageID == o.pageID && slotID == o.slotID;
     }
 };
 
@@ -22,10 +22,10 @@ class RecordManager {
 public:
     RecordManager(PageManager& pm, const Schema& schema);
 
-    // Вставить запись, вернуть её RecordID
+    // Вставить запись, вернуть её RecordId
     RecordID insert(const std::vector<Value>& record);
 
-    // Прочитать запись по RecordID
+    // Прочитать запись по RecordId
     std::vector<Value> fetch(RecordID rid);
 
     // Обновить запись
@@ -46,12 +46,12 @@ private:
     page_id_t find_page_with_space(size_t needed_bytes);
 
     // Работа со слотами внутри страницы
-    int16_t get_slot_count(const Page& page);
-    int16_t get_free_offset(const Page& page);
-    void write_slot(Page& page, int16_t slot, int16_t offset, int16_t size);
-    int16_t get_slot_offset(const Page& page, int16_t slot);
-    int16_t get_slot_size(const Page& page, int16_t slot);
+    int16_t  get_slot_count(const Page& page);
+    int16_t  get_free_offset(const Page& page);
+    void     write_slot(Page& page, int16_t slot, int16_t offset, int16_t size);
+    int16_t  get_slot_offset(const Page& page, int16_t slot);
+    int16_t  get_slot_size(const Page& page, int16_t slot);
 };
 
 
-#endif // DBMS_PAIN_RECORDMANAGER_H
+#endif //DBMS_PAIN_RECORDMANAGER_H
