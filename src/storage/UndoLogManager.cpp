19aeb65767518c5854d6f7e01cacdab928ed1ed1
Vika <vika.yefremova.06@gmail.com>
2026-05-25 16:07:34 +0300

    UndoLogmanager interface and forbid for index manager to be copied

---
+++ src/storage/UndoLogManager.cpp
@@ -0,0 +1,5 @@
+#include "UndoLogManager.h"
+
+// UndoLogManager::UndoLogManager(const std::string& LogfilePath) : filePath_(LogfilePath) {
+//     std::lock_guard<std::mutex> lock(this->mutex_);
+// }
